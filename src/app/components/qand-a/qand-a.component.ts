2106092c73f0978f2ac1098a863060cf57d1be1e
lohithaarva <lohitha.arva@gmail.com>
2018-12-01 19:44:18 +0530

    31 Nov

--- src/app/components/qand-a/qand-a.component.ts
+++ src/app/components/qand-a/qand-a.component.ts
@@ -42,8 +42,17 @@ export class QandAComponent implements OnInit, OnDestroy {
   private replySecondLevel;
   private secondReply = [];
   public thirdReply=[];
+  private xyz;
+  private value;
+  private avgRate;
+  // private keyDownArray: boolean = false;
+  // private keyDownArrayOnce:boolean = false;
+  // private keyDownArrayTwice:boolean = false;
+  private replies=[]
+  private replyObj;
   constructor(public questionanswer: QuestionService, public route: ActivatedRoute, private router: Router) { }
 
+  
   ngOnInit() {
     this.image = localStorage.getItem('imageUrl');
     this.img = environment.apiUrl;
@@ -81,11 +90,17 @@ export class QandAComponent implements OnInit, OnDestroy {
           if (this.qandAData.questionAndAnswerNotes[0] != undefined) {
             this.addQuestions.push(this.qandAData.questionAndAnswerNotes[0]);
             this.likeCount = this.qandAData.questionAndAnswerNotes[0].like.length
+            this.xyz = this.qandAData.questionAndAnswerNotes;
             for (let i = 1; i < this.qandAData.questionAndAnswerNotes.length; i++) {
               if(this.qandAData.questionAndAnswerNotes[i].parentId===this.addQuestions[0].id){
               this.replyOnce.push(this.qandAData.questionAndAnswerNotes[i])
               }
             }
+        for (let i = 1; i < this.qandAData.questionAndAnswerNotes.length; i++) {
+          if (this.qandAData.questionAndAnswerNotes[0].id === this.qandAData.questionAndAnswerNotes[i].parentId) {
+            this.replies.push(this.qandAData.questionAndAnswerNotes[i]);
+          }
+        }
           }
           console.log("question and answer successfull", this.qandAData);
         })
@@ -112,11 +127,13 @@ export class QandAComponent implements OnInit, OnDestroy {
     let requestbody = {
       "like": true
     }
-    this.questionanswer.addLikes(requestbody, ques.id)
+    this.questionanswer.addLikes(requestbody, ques)
       .pipe(takeUntil(this.destroy$))
       .subscribe(data => {
         this.count = data['data']['details'].count;
       })
+      console.log(ques,'id of like is here');
+      
   }
 
   ratingAnswer(value, event) {
@@ -180,6 +197,17 @@ export class QandAComponent implements OnInit, OnDestroy {
     return true;
   }
 
+  averageRating(rateArray) {
+    this.value = 0;
+    if (rateArray.length != 0) {
+    for (let i = 0; i < rateArray.length; i++) {
+    this.value += rateArray[i].rate
+    }
+    this.avgRate = this.value / rateArray.length;
+    return this.avgRate;
+    }
+    }
+
   ngOnDestroy() {
     this.destroy$.next(true);
     // Now let's also unsubscribe from the subject itself:
@@ -205,3 +233,206 @@ export class QandAComponent implements OnInit, OnDestroy {
 
 
 
+// export class QandAComponent implements OnInit, OnDestroy {
+//   @ViewChild('question') private questionAksed: ElementRef;
+//   @ViewChild('reply') private replyDone: ElementRef;
+//   destroy$: Subject<boolean> = new Subject<boolean>();
+//   constructor(private route: ActivatedRoute,
+//     private noteService: NoteService,
+//     private qService: QuesAndAnsService,
+//     private router: Router) { }
+//   private noteId;
+//   private title;
+//   private description;
+//   private checklist;
+//   private questionStatus = false;
+//   private question = [];
+//   private questionValue;
+//   private isReply = false;
+//   URL = environment.URL;
+//   noteDetails: Note[] = [];
+//   private replies=[]
+//   private rate;
+//   private replyObj;
+ 
+  
+  
+//   ngOnInit() {
+//     this.route.params.subscribe(
+//       (params: Params) => {
+//         this.noteId = params['noteId']
+//       })
+//       this.getNoteDetails();    
+//   }
+//   getNoteDetails(){
+//     this.noteService.getNOteDetails(this.noteId)
+//       .pipe(takeUntil(this.destroy$))
+//       .subscribe(response => {
+//         this.noteDetails = response['data'].data
+//         console.log(this.noteDetails);
+//         this.title = this.noteDetails[0].title;
+//         if (this.noteDetails[0].description != undefined) {
+//           this.description = this.noteDetails[0].description;
+//         }
+//         if (this.noteDetails[0].noteCheckLists != undefined) {
+//           this.checklist = this.noteDetails[0].noteCheckLists
+//         }
+//         if (this.noteDetails[0].questionAndAnswerNotes !== undefined) {
+//           for (let i = 0; i < this.noteDetails[0].questionAndAnswerNotes.length;i++){
+//             this.question.push(this.noteDetails[0].questionAndAnswerNotes[i])
+//           }
+//         }
+//         if (this.question.length !== 0) {
+//           this.questionStatus = true;
+//         }
+        
+//         for (let i = 1; i < this.question.length; i++) {
+//           if (this.question[0].id === this.question[i].parentId) {
+//             this.replies.push(this.question[i]);
+//           }
+//         }
+//     })
+
+//   }
+//   private imag
+//   private imagpath
+//   private rateDisp;
+//   private userRating
+//   imageFormation(ques){
+  
+//     this.imag = ques.user.imageUrl;
+//     this.imagpath = this.URL + this.imag;
+//     return true;
+//   }
+//   private liked = false;
+//   isliked(ques) {
+//     this.liked = false;
+//     for (let i = 0; i < ques['like'].length; i++) {
+//       if (ques.like[i].userId == localStorage.getItem('userId') && ques.like[i].like == true) {
+//         this.liked = true;
+//         return true;
+//       }
+//     }
+//     return true;
+
+//   }
+
+//   private lykC;
+//   likeDisplay(ques){
+//     this.lykC=0;
+//     for (let i = 0; i < ques.like.length;i++){
+//       if(ques.like[i].like==true){
+//         this.lykC=this.lykC+1;
+//       }
+//     }
+//     return true;
+//   }
+//   lengthCheck(){
+//     if(this.question.length>1){
+//       return true;
+//     }
+//     return false;
+//   }
+//   private rX=[]
+//   hasReply(ques)
+// {
+//     this.rX=[]
+//     for (let i = 1; i < this.question.length; i++) {
+//       if (ques.id === this.question[i].parentId) {
+//         this.rX.push(this.question[i])
+//         }
+//     }
+//    return true;
+// }  
+// private rZ;
+// hasReplysecnd(ques){
+// this.rZ=[];
+//   for (let i = 1; i < this.question.length; i++) {
+//     if (ques.id === this.question[i].parentId) {
+//       this.rZ.push(this.question[i])
+
+//     }
+//   }
+//   return true;
+// }
+// addQuestion(e) {
+//       if (e.keyCode === 13) {
+//       this.questionValue = this.questionAksed.nativeElement.innerHTML;
+//       this.addQuestionToNote();
+//       this.questionAksed.nativeElement.innerHTML = '';
+//     }
+// }
+//   addQuestionToNote() {
+//     let RequestBody = {
+//       "message": this.questionValue,
+//       "notesId": this.noteId
+//     }
+//     this.qService.addAquestion(RequestBody)
+//       .pipe(takeUntil(this.destroy$))
+//       .subscribe(response => {
+//         this.questionStatus = false;
+//         this.getNoteDetails();
+//       })
+//   }
+//   closeQandA() {
+//     this.router.navigate(['home'])
+//   }
+  
+//   reply(replyOBJ) {
+//     this.replyObj=replyOBJ;
+//     console.log(this.replyObj);
+//     this.isReply = true;
+//   }
+//   sendReply() {
+//     console.log(this.replyDone.nativeElement.innerHTML);
+//     let RequestBody={
+//       "message": this.replyDone.nativeElement.innerHTML
+//     }
+//     this.qService.addReply(RequestBody,this.replyObj.id)
+//       .pipe(takeUntil(this.destroy$))
+//       .subscribe(response=>{})
+//   }
+//   like(ques,flag){
+//     let requestbody={
+//       "like": flag
+//     }
+//     this.qService.addLike(requestbody, ques.id)
+//       .pipe(takeUntil(this.destroy$))
+//       .subscribe(response => {
+//         this.liked=flag;
+//       })
+//   }
+//   rating1(rate,ques){
+//     console.log(rate);
+//     let request={
+//       "rate":rate
+//     }
+//     this.qService.addRating(request, ques.id)
+//       .pipe(takeUntil(this.destroy$))
+//       .subscribe(response=>{})
+//   }
+//   ratingDisp(ques) {
+//     this.rateDisp=0;
+//     this.userRating =0;
+//     let cal = ques.rate.length;
+//     let count = 0;
+//     if (cal !== 0) {
+//       for (let i = 0; i < cal; i++) {
+//         count = count + ques.rate[i].rate
+//         if (ques.rate[i].userId === localStorage.getItem('userId')) {
+//           this.userRating = ques.rate[i].rate;
+//         }
+//       }
+
+//       this.rateDisp = count / cal
+//       console.log(this.rateDisp);
+
+//     }
+//     return true;
+//   }
+//   ngOnDestroy() {
+//     this.destroy$.next(true);
+//     this.destroy$.unsubscribe();
+//   }
+
+// }
